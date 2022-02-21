76635b73756b90ea5a90da004ce84170ee2f217a
Pranay Rane <43698603+pranayzv@users.noreply.github.com>
2022-02-21 18:24:29 +0530

    Add files via upload

---
+++ src/GoodGreeting.java
@@ -0,0 +1,9 @@
+
+public class GoodGreeting implements Gretting {
+
+	@Override
+	public void perform() {
+		System.out.println("Hello World!");
+	}
+
+}
---
+++ src/Gretting.java
@@ -0,0 +1,7 @@
+
+@FunctionalInterface
+public interface Gretting {
+	
+	void perform();
+
+}
